2a3380ff9768e92de646fd15346e36ffb44f2aee
JinHyeRim <wlsgpfla92@gmail.com>
2021-05-10 21:19:54 +0900

    시간이 넘 오래걸린다... 강의 듣고 풀어보기

--- Programmers/01_Greedy/Joystick.py
+++ Programmers/01_Greedy/Joystick.py
@@ -1,18 +1,3 @@
-# ▲ - 다음 알파벳
-# ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
-# ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
-# ▶ - 커서를 오른쪽으로 이동
-
-# 예를 들어 아래의 방법으로 "JAZ"를 만들 수 있습니다.
-
-# - 첫 번째 위치에서 조이스틱을 위로 9번 조작하여 J를 완성합니다.
-# - 조이스틱을 왼쪽으로 1번 조작하여 커서를 마지막 문자 위치로 이동시킵니다.
-# - 마지막 위치에서 조이스틱을 아래로 1번 조작하여 Z를 완성합니다.
-# 따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다.
-
-#   name	  return
-# "JEROEN"	  56
-# "JAN"	      23
 
 def solution(name):
     cursorCount = 0
@@ -45,45 +30,25 @@ def solution(name):
 
     # cursorCount 계산(오른쪽으로 갔다가 돌아오는 경우)
     if name.find("A") != -1 and name.count("A") > 1:
-      # preIdx = name.find("A")
-      # nextIdx = name.find("A", preIdx + 1)
-      # while nextIdx != -1:
-      #   print(f"preIdx: {preIdx}, nextIdx: {nextIdx}")
-      #   aLength = nextIdx - preIdx + 1
-      #   if name[preIdx:nextIdx + 1] == "A" * aLength:
-      #     # break 안걸고 길이 비교해서 제일 긴걸로 하도록 해야될거 같긴 한데...귀찮다...
-      #     print("if?")
-      #     frontCount = 2 * (len(name[:preIdx]) - 1)
-      #     backCount = len(name[nextIdx:]) - 1
-      #     cursorCount = min(cursorCount, frontCount + backCount)
-      #     break
-      #   else:
-      #     print("else?")
-      #     preIdx = int(nextIdx)
-      #     nextIdx = name.find("A", preIdx + 1)
       startIdx = name.find("A")
-      
-      while :
-        endIdx = int(startIdx) + 1  #init
-        if endIdx == startIdx + 1:
+      endIdx = name.find("A", startIdx + 1)
+      while True:
+        if endIdx == startIdx + 1 and endIdx != len(name) - 1:
           # 연속될 때
-          print("if")
-          endIdx = name.find("A", endIdx + 1)
-          for char in rnage(endIdx):
+          for char in name[endIdx + 1:]:
             if char == "A":
-
+              endIdx += 1
+              continue
+            else:
+              break
+          break
         else:
           # 연속이 아닐 때
-          print("else")
           if endIdx == -1:
             break
           startIdx = int(endIdx)
           endIdx = name.find("A", endIdx + 1)
 
-      print(f"startIdx: {startIdx}")
-      print(f"endIdx: {endIdx}")
-      print(f"name[startIdx:endIdx + 1]: {name[startIdx:endIdx + 1]}")
-      
       if endIdx != -1:
         frontCount = 2 * (len(name[:startIdx]) - 1)
         backCount = len(name[endIdx:]) - 1
