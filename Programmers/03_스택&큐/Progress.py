956a036b77f812cb1055926cdab68bf9e2bd2b16
진혜림 <wlsgpfla92@gmail.com>
2021-06-02 21:21:25 +0900

    권시내 그녀는 신인가

--- Programmers/03_스택&큐/Progress.py
+++ Programmers/03_스택&큐/Progress.py
@@ -41,24 +41,74 @@
 from collections import deque
 import math
 
+# def solution(progresses, speeds):
+#     answer = []
+#     dProgresses = deque(progresses)
+#     dSpeeds = deque(speeds)
+#
+#     while len(dProgresses) > 0:
+#         progress = dProgresses.popleft()
+#         speed = dSpeeds.popleft()
+#
+#         if progress < 100:
+#             remain = math.ceil((100 - progress) / speed)
+#             for i, value in enumerate(dProgresses):
+#                 dProgresses[i] = (remain * dSpeeds[i]) + value
+#             answer.append(0)
+#
+#         answer[-1] += 1
+#
+#     return answer
+
+# 정확성  테스트
+# 테스트 1 〉	통과 (0.01ms, 10.2MB)
+# 테스트 2 〉	통과 (0.09ms, 10.2MB)
+# 테스트 3 〉	통과 (0.08ms, 10.1MB)
+# 테스트 4 〉	통과 (0.05ms, 10.2MB)
+# 테스트 5 〉	통과 (0.01ms, 10.1MB)
+# 테스트 6 〉	통과 (0.02ms, 10.3MB)
+# 테스트 7 〉	통과 (0.08ms, 10.2MB)
+# 테스트 8 〉	통과 (0.02ms, 10.4MB)
+# 테스트 9 〉	통과 (0.06ms, 10.2MB)
+# 테스트 10 〉	통과 (0.06ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.2MB)
+
 def solution(progresses, speeds):
     answer = []
-    dProgresses = deque(progresses)
-    dSpeeds = deque(speeds)
+    count = 0
+    remain = 1
 
-    while len(dProgresses) > 0:
-        progress = dProgresses.popleft()
-        speed = dSpeeds.popleft()
+    for i in range(len(progresses)):
+        progress = progresses[i]
+        speed = speeds[i]
 
-        if progress < 100:
-            remain = math.ceil((100 - progress) / speed)
-            for i, value in enumerate(dProgresses):
-                dProgresses[i] = (remain * dSpeeds[i]) + value
-            answer.append(0)
+        print(f"progress {progress}, progress + (remain * speed) {progress + (remain * speed)}")
+        if progress + (remain * speed) < 100:
+            if count > 0:
+                answer.append(count)
+            remain = math.ceil((100 - progress) / speeds[i])
+            count = 1
+            # for i, value in enumerate(dProgresses):
+            #     dProgresses[i] = (remain * dSpeeds[i]) + value
+        else:
+            count += 1
 
-        answer[-1] += 1
+    answer.append(count)
 
     return answer
+
+# 정확성  테스트
+# 테스트 1 〉	통과 (0.01ms, 10.2MB)
+# 테스트 2 〉	통과 (0.03ms, 10.2MB)
+# 테스트 3 〉	통과 (0.02ms, 10.2MB)
+# 테스트 4 〉	통과 (0.02ms, 10.1MB)
+# 테스트 5 〉	통과 (0.01ms, 10.2MB)
+# 테스트 6 〉	통과 (0.01ms, 10.3MB)
+# 테스트 7 〉	통과 (0.02ms, 10.3MB)
+# 테스트 8 〉	통과 (0.01ms, 10.2MB)
+# 테스트 9 〉	통과 (0.02ms, 10.2MB)
+# 테스트 10 〉	통과 (0.02ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.3MB)
 
 print(solution([93, 30, 55], [1, 30, 5]))     # [2, 1]
 # print()
